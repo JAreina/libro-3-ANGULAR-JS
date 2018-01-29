02c12f7a9f78a4d43753f547b451fdaca2600993
JAreina <jareinafdez@gmail.com>
2018-01-29 10:23:44 +0100

    anchorScroll

--- 15-anchorScroll/app/js/Controllers/AnchorScrollController.js
+++ 15-anchorScroll/app/js/Controllers/AnchorScrollController.js
@@ -1,7 +1,9 @@
 angular.module('jareina',[])
-.controller('AnchorScrollController', function($scope,$anchorScroll){
+.controller('AnchorScrollController', function($scope,$anchorScroll,$location){
   $scope.irA= function(id){
+
     var nuevaId = `contenido${id}`;
+    $location.hash(nuevaId);//nueva direccion en barra url
         $anchorScroll(nuevaId);
   }
 })
