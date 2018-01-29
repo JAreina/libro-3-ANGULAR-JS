f194c0bd862ddb67f3d8f7b7010ee6f9a8d0dc77
JAreina <jareinafdez@gmail.com>
2018-01-29 10:35:14 +0100

    anchorscroll

--- 15-anchorScroll/app/js/app.js
+++ 15-anchorScroll/app/js/app.js
@@ -1,4 +1,8 @@
 
 "use strict";
 // Declaramos la aplicacion y definimos sus dependencias
-angular.module('jareina', []);
+angular.module('jareina', [])
+.run(['$anchorScroll',function ($anchorScroll) {
+        $anchorScroll.yOffset = 100;
+        
+      }]);
