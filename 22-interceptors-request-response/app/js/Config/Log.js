f80a35286878734adc268f0fb329daf361c04687
JAreina <jareinafdez@gmail.com>
2018-01-31 17:49:43 +0100

    directives ng-class

--- 22-interceptors-request-response/app/js/Config/Log.js
+++
@@ -1,6 +0,0 @@
-angular.module('jareina')
-.config(Log);
-Log.$inject =['$logProvider'];
-function Log($logProvider){
-  $logProvider.debugEnabled([true]);
-}
--- 22-interceptors-request-response/app/js/Controllers/AnchorScrollController.js
+++
@@ -1,9 +0,0 @@
-angular.module('jareina',[])
-.controller('AnchorScrollController', function($scope,$anchorScroll,$location){
-  $scope.irA= function(id){
-
-    var nuevaId = `contenido${id}`;
-    $location.hash(nuevaId);//nueva direccion en barra url
-        $anchorScroll(nuevaId);
-  }
-})
