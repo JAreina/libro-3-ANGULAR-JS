64cd415927476e5a0a77469caf3985c3eb32ebcf
JAreina <jareinafdez@gmail.com>
2018-02-05 15:34:43 +0100

    ng-resource

--- 31-ng-resource-peticiones-http/app/js/Controllers/mensajesController.js
+++ 31-ng-resource-peticiones-http/app/js/Controllers/mensajesController.js
@@ -1,11 +1,31 @@
 angular.module('jareina')
   .controller('mensajesController', ['$scope', 'mensajesService',
     function($scope, mensajesService) {
+
+
       mensajesService.query(function(datos) {
         $scope.mensajes = datos;
         console.log(datos)
       }, function(err) {
         console.error('Error ' + err.status + ': ' + err.data.mensaje);
       });
+
+      //un usuario solo
+      $scope.uno = mensajesService.get({
+        usuario: 1
+      })
+
+
+      //POST
+      $scope.nuevo = function() {
+        mensajesService.save( $scope.msg, function(res) {
+          $scope.mensajes.push(res);
+          $scope.msg = {};
+          console.log("AÑADIDO " + res)
+        }, function(error) {
+          console.log(error)
+        });
+      }
+
     }
   ]);
--- 31-ng-resource-peticiones-http/app/js/Services/mensajesService.js
+++ 31-ng-resource-peticiones-http/app/js/Services/mensajesService.js
@@ -1,5 +1,18 @@
 angular.module('jareina')
 .factory('mensajesService', ['$resource', function ($resource) {
 // return $resource('./api/generated.json');
- return $resource('http://localhost:3000/usuarios/:usuario',{usuario: "@usuario"});
+ return $resource('http://localhost:3000/usuarios/:usuario',
+                                                          {usuario: "@usuario"},
+                                                                                       null,
+                              { 'get':    {method:'GET'},
+                                 'save':   {method:'POST'},
+                                 'query':  {method:'GET', isArray:true},
+                                 'remove': {method:'DELETE'},
+                                 'delete': {method:'DELETE'}
+                               })
+
+
+
+
+
  }]);
